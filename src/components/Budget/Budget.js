eeb1395c18db8a7910f068f662d79a9c7768884a
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-29 16:51:39 -0600

    update budget

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -8,16 +8,22 @@ import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
 import { connect } from 'react-redux';
+import { requestUserData } from './../../redux/userReducer'
+
 
 class Budget extends Component {
+  componentDidMount() {
+    this.props.requestUserData()
+  }
 
   render() {
     const { loading } = this.props.budget;
+    const { firstName, lastName } = this.props.user
     return (
       <Background>
         {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName} />
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
@@ -41,4 +47,4 @@ function mapStateToProps(reduxState) {
   }
 }
 
-export default connect(mapStateToProps)(Budget);
+export default connect(mapStateToProps, { requestUserData })(Budget);
